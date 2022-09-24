5d406c35836378a77a1e2250ce73e95b089782c6
209 <209@academicos.cesde.edu.co>
2022-09-24 13:15:42 -0500

    se crea la suma de el arreglo

--- Reto1.py
+++ Reto1.py
@@ -4,5 +4,25 @@ def llenarArray ():
         numero = int(input("Digita un numero entero: "))
         numeros.append(numero)
     return numeros
-llenarArray()
+
+numeros = llenarArray()
+
+def sumar(numeros):
+    operacion = 0
+    # print(numeros)
+    for i in numeros:
+        operacion += i
+    return(operacion)    
+         
+sumar(numeros)
+
+# longitud = numeros.count
+
+# def dividir(longitud,operacion):
+#     divisor = operacion / longitud
+   
+#     print(divisor)
+
+# dividir(longitud)        
+
     
