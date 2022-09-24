5ec74916556caca50ad20608227ce502603948b3
209 <209@academicos.cesde.edu.co>
2022-09-24 12:25:05 -0500

    Se crea la funcion con el bucle llenando el arreglo

---
+++ Reto1.py
@@ -0,0 +1,8 @@
+def llenarArray ():
+    numeros = []
+    for i in range(2):
+        numero = int(input("Digita un numero entero: "))
+        numeros.append(numero)
+    return numeros
+llenarArray()
+    
