afc76caa16d0ed033767b11c153171a57ae4658c
SebasMejia1 <sebastianmejiasanchez1030@gmail.com>
2022-09-24 21:07:51 -0500

    Se corrigen funciones de sumar y calcular promedio del Reto1

--- Reto1.py
+++ Reto1.py
@@ -1,19 +1,21 @@
 def llenarArray ():
     numeros = []
-    for i in range(2):
+    for i in range(10):
         numero = int(input("Digita un numero entero: "))
         numeros.append(numero)
     return numeros
 
 def sumarNumeros(numeros):
-    for i in range (numeros):
-        suma += i
+    suma = 0
+    for i in range (len(numeros)):
+        suma += numeros[i]
     return suma
 
 def calcularPromedio(suma,numeros):
-    cantNumeros = numeros.count
+    cantNumeros = len(numeros)
     promedio = suma/cantNumeros
     print(f'El promedio de los numeros es de: {promedio}')
+    
 numeros = llenarArray()
 suma = sumarNumeros(numeros)
 calcularPromedio(suma,numeros)
